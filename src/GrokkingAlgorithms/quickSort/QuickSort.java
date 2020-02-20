b1f91f6012420ed45b444b36bce935dd4f35a12f
Vitiatte <vpavliuk@varteq.com>
2020-02-20 18:59:15 +0200

    Add quick sort that uses List under the hood

--- src/GrokkingAlgorithms/quickSort/QuickSort.java
+++ src/GrokkingAlgorithms/quickSort/QuickSort.java
@@ -3,46 +3,58 @@ package GrokkingAlgorithms.quickSort;
 import util.DataCreator;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int[] g = DataCreator.getUnsortedIntegerArray(20, false);
-        int[] sortedArr = sort(g);
+        int[] g = DataCreator.getUnsortedIntegerArray(100000, true);
+        List<Integer> list = Arrays.stream(g)
+                .boxed()
+                .collect(Collectors.toList());
 
-        for (int i : g) {
-            System.out.print(i);
+        for (int i : list) {
+            System.out.print(i + " ");
         }
-
+        list = sort(list);
         System.out.println();
 
-        for (int i : sortedArr) {
-            System.out.print(i);
+        for (int i : list) {
+            System.out.print(i + " ");
         }
+        System.out.println();
     }
 
-    private static int[] sort(int[] arr) {
-        if (arr.length < 2) {
-            return arr;
+    private static List<Integer> sort(List<Integer> list) {
+        if (list.size() < 2) {
+            return list;
         }
-        int base = arr[0];
-        int[] smaller =  new int[arr.length], bigger = new int[arr.length];
+        int base = list.get(0).intValue();
+        List<Integer> smaller = new ArrayList<>();
+        List<Integer> bigger = new ArrayList<>();
         //check all elements with base and place it in bigger or smaller
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] >= base) {
-                bigger[bigger.length] = arr[i];
+        int biggerIndex=0, smallerIndex=0;
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i).intValue() >= base) {
+                bigger.add(list.get(i));
             } else {
-                smaller[smaller.length] = arr[i];
+                smaller.add(list.get(i));
             }
         }
-        return combine(smaller, base, bigger);
+        bigger = sort(bigger);
+        smaller = sort(smaller);
+        smaller.add(Integer.valueOf(base));
+        smaller.addAll(bigger);
+        return smaller;
     }
 
-    private static int[] combine(int[] smaller, int base, int[] bigger) {
-        int[] combinedArr = new int[smaller.length + 1 + bigger.length];
-        System.arraycopy(smaller, 0, combinedArr, 0, smaller.length);
-        combinedArr[smaller.length] = base;
-        System.arraycopy(bigger, 0, combinedArr, smaller.length + 1, bigger.length);
-        return combinedArr;
-    }
+//    private static int[] combine(int[] smaller, int base, int[] bigger) {
+//        int[] combinedArr = new int[smaller.length + 1 + bigger.length];
+//        System.arraycopy(smaller, 0, combinedArr, 0, smaller.length);
+//        combinedArr[smaller.length] = base;
+//        System.arraycopy(bigger, 0, combinedArr, smaller.length + 1, bigger.length);
+//        return combinedArr;
+//    }
 }
